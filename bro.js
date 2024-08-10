3de8b157aa3b851777aa48ce72baa8285e36454d
mbanefoobiajulu <dibiaezueobiajulu@yahoo.com>
2024-08-10 20:10:29 +0100

    WELL ARRANGED COUNTER PROGRAM

--- bro.js
+++ bro.js
@@ -1,19 +1,17 @@
+const lowerbtn = document.getElementById("lowerbtn");
+const resetbtn = document.getElementById("resetbtn");
+const higherbtn = document.getElementById("higherbtn");
+const countlabel = document.getElementById("countlabel");
 
- const lowerbtn = document.getElementById("lowerbtn")
- const resetbtn = document.getElementById("resetbtn")
- const higherbtn = document.getElementById("higherbtn")
- const countlabel = document.getElementById("countlabel")
- 
- 
- lowerbtn.onclick = function(){
-    count--;
-    countlabel.textContent= count;
- }
- higherbtn.onclick = function(){
-    count++;
-    countlabel.textContent= count;
- }
- resetbtn.onclick = function(){
-    count = 0;
-    countlabel.textContent= count;
- }
+lowerbtn.onclick = function () {
+  count--;
+  countlabel.textContent = count;
+};
+higherbtn.onclick = function () {
+  count++;
+  countlabel.textContent = count;
+};
+resetbtn.onclick = function () {
+  count = 0;
+  countlabel.textContent = count;
+};
